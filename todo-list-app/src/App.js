06695d2469edc838b7a020178e2c3a6e9c1449b2
LinoVarela <linorafaelcarvalhovarela@gmail.com>
2024-09-22 20:07:50 +0100

    todo list delete task

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,32 +1,71 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 function App() {
-  
-  const [todoList,setTodoList] = useState([]) // pass empty array
-  const [currentTask,setCurrentTask] = useState("")
-  
+
+  const [todoList, setTodoList] = useState([]) // pass empty array
+  const [currentTask, setCurrentTask] = useState("")
+
+  const inputTask = useRef(null)
 
   const addTask = () => {
-    setTodoList([...todoList,currentTask]) //...todolist (What was on the list previously + new task)
+    setTodoList([...todoList, { task: currentTask, completed: false }]) //...todolist (What was on the list previously + new task), task e completed -> objeto com 2 propriadades (task e estado)
+    inputTask.current.value = "";
+    setCurrentTask("")
     console.log(todoList)
-  }
+  };
+
+
+  function deleteTask(taskToDelete) {
+    setTodoList(todoList.filter((task) => {
+      return task.task !== taskToDelete;
+    }))
+  };
+
+  function completeTask(taskToComplete) {
+    setTodoList(
+      todoList.map((task) => {
+      //if task is the taskto completed, make it true or else
+      return task.task == taskToComplete
+        ? { task: taskToComplete, completed: true }
+        : { task: task.task, completed: task.completed ? true : false }; //if task was previously keep it true, if not false 
+    }))
+  };
+
+
 
   return (
-    <div className="App"><h1>TODO LIST</h1>
+    <div className="App">
+      <h1>TODO LIST</h1>
       <div>
-        <input type="text" 
-        placeholder="Task..." 
-        onChange={(event)=>
-          {setCurrentTask(event.target.value)}}> 
+        <input
+          ref={inputTask}
+          type="text"
+          placeholder="Task..."
+          onChange={(event) => { setCurrentTask(event.target.value) }}>
         </input>
         <button onClick={addTask}>Add task</button>
       </div>
-      <hr></hr>
-
+      <hr />
+      <ul>
+        {todoList.map((val, key) => {  //map looops through list/array
+          return (
+            <div id="task">
+              <li key={key}>{val.task}</li>
+              <button onClick={() => completeTask(val.task)}>Completed</button>
+              <button onClick={() => deleteTask(val.task)}>X</button>
+              {val.completed ? (
+                <h1>Task Completed</h1>
+              ) : (
+                <h1>Task not completed</h1>
+              )}
+            </div>
+          );
+        })}
+      </ul>
     </div>
-    );
+  );
 }
 
 export default App;
