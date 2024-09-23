ca2dbada1fa65dd3955dde815a3e9d915d060235
LinoVarela <linorafaelcarvalhovarela@gmail.com>
2024-09-23 16:21:49 +0100

    States & context

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -2,20 +2,26 @@ import './App.css';
 import Menu from './components/Menu'
 import { useState } from 'react';
 
-
+import { GameStateContext } from './helpers/Contexts';
 
 
 //['menu','playing','finished']
 function App() {
 
-  const [gameState,setGameState] = useState(""); //will represent the state we are in the game
+  const [gameState, setGameState] = useState("menu"); //will represent the state we are in the game
 
+
+
+  //GameStateContext helps to see the state where we are in the game
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <Menu></Menu>
+      <GameStateContext.Provider value={{gameState,setGameState}}> 
+        {gameState === "menu" && <Menu />}
+      </GameStateContext.Provider>
+
     </div>
   );
-} 
+}
 
 export default App;
