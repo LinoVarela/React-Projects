b67811c37223a81deddba21e95d1a320311bbf93
LinoVarela <linorafaelcarvalhovarela@gmail.com>
2024-09-23 17:39:48 +0100

    Quiz Page and ye

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Menu from './components/Menu'
+import Quiz from './components/Quiz';
 import { useState } from 'react';
-
 import { GameStateContext } from './helpers/Contexts';
 
 
@@ -9,6 +9,7 @@ import { GameStateContext } from './helpers/Contexts';
 function App() {
 
   const [gameState, setGameState] = useState("menu"); //will represent the state we are in the game
+  const [userName,setUserName] = useState("")
 
 
 
@@ -16,9 +17,13 @@ function App() {
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <GameStateContext.Provider value={{gameState,setGameState}}> 
+      <GameStateContext.Provider value={{gameState,setGameState,userName,setUserName}}> 
         {gameState === "menu" && <Menu />}
+        {gameState === "playing" && <Quiz/>}
+      
       </GameStateContext.Provider>
+
+
 
     </div>
   );
--- quiz-app/src/components/Menu.js
+++ quiz-app/src/components/Menu.js
@@ -1,15 +1,23 @@
 import '../App.css';
-
+import { useContext } from 'react';
+import { GameStateContext } from '../helpers/Contexts';
 
 function Menu() {
+
+    const { GameState, setGameState , userName, setUserName} = useContext(GameStateContext);
+    
+
+
     return (
         <div className="Menu">
             <label>Enter your Name:</label>
-            <input type="text" placeholder="Ex: Lino"></input>
-            <button>Start Quiz</button>
+            <input type="text" placeholder="Ex: Lino" onChange={(event)=>setUserName(event.target.value)}></input>
+            <button onClick={() => { setGameState('playing') }}>Start Quiz</button> 
+
+
         </div>
     );
 }
 
 
-export default Menu;
+export default Menu;    
